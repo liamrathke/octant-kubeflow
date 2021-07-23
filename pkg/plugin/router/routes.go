7baf96ff7debfe6d10fa4d005475664862d5066f
Liam Rathke <liam.rathke@gmail.com>
2021-07-23 17:21:15 -0400

    Update routes.go

--- pkg/plugin/router/routes.go
+++ pkg/plugin/router/routes.go
@@ -18,9 +18,36 @@ package router // import "github.com/liamrathke/octant-kubeflow/pkg/plugin/route
 
 import (
 	"github.com/vmware-tanzu/octant/pkg/plugin/service"
+	"github.com/vmware-tanzu/octant/pkg/view/component"
 )
 
+var r *service.Router
+
 func InitRoutes(router *service.Router) {
-	router.HandleFunc("", rootHandler)
-	router.HandleFunc("/dashboard", dashboardHandler)
+	r = router
+	router.HandleFunc("/", func(request service.Request) (component.ContentResponse, error) {
+		return component.ContentResponse{}, nil
+	})
+	routeHelper("", rootHandler)
+	routeHelper("/dashboard", dashboardHandler)
+}
+
+func routeHelper(routePath string, handleFunc service.HandleFunc) {
+	// var wrapMiddleware service.HandleFunc
+	// wrapMiddleware = func(request service.Request) {
+	// 	return handleMiddleware(request, handleFunc)
+	// }
+	r.HandleFunc(routePath, func(request service.Request) {
+		return handleMiddleware(request, handleFunc)
+	})
+}
+
+func handleMiddleware(request service.Request, handleFunc service.HandleFunc) (component.ContentResponse, error) {
+	// Middleware code goes here
+	// If Kubeflow not installed, redirect to information page (later installation page)
+	view, err := handleFunc(request)
+	if err != nil {
+		return component.EmptyContentResponse, err
+	}
+	return view, nil
 }
